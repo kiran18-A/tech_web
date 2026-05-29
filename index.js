5c6b985b1ff49ef7cebd6245efd703dabac93b5c
kiran-A <kiranaglawe18@gmail.com>
2026-05-30 00:22:48 +0530

    First time

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(8080, () => {
+    console.log("Server running on port 8080");
+});
