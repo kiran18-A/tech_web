a19fa0f1ba0ea35d148bfbe15a202cf1dc5a5dbf
kiran-A <kiranaglawe18@gmail.com>
2026-06-07 17:10:55 +0530

    for mail

--- mailer.js
+++ mailer.js
@@ -5,6 +5,7 @@ const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
+    family: 4,
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASSWORD
